d3154d2f0d45b7ca181faea35a021224f0ef2ba8
Léo Bix <leobix@dhcp-10-29-69-40.dyn.MIT.EDU>
2022-02-07 17:49:56 -0500

    add metrics

---
+++ src/metrics.jl
@@ -0,0 +1,59 @@
+function R2(y_true, y_test)
+    SSR = sum(abs2.(y_true.-y_test))
+    SST = sum(abs2.(y_true.-mean(y_true)))
+    return 1 - SSR/SST
+end
+
+function R2_err(err, y_true)
+    SSR = sum(abs2.(err))
+    SST = sum(abs2.(y_true.-mean(y_true)))
+    return 1 - SSR/SST
+end
+
+function get_metrics(err, yt_true)
+    #TODO ADD saving mechanism
+    MAE = mean(err)
+    CVAR_05 = compute_CVaR(err, 0.05)
+    CVAR_15 = compute_CVaR(err, 0.15)
+    R2 = R2_err(err, yt_true)
+    MAPE = 100*sum(abs.(err./yt_true))/size(err)[1]
+    RMSE = sqrt(sum(abs2.(err))/size(err)[1])
+    println("MAE : ", MAE)
+    println("MAPE : ", MAPE)
+    println("RMSE : ", RMSE)
+    println("R2 : ", R2)
+    println("CVAR 0.05 :", CVAR_05)
+    println("CVAR 0.15 :", CVAR_15)
+end
+
+
+function compute_CVaR(errs, α_risk)
+#     '''
+#     Compute the Conditional Value at Risk
+#     :param X: Either the data matrix X or the errors
+#     :param y: the target values
+#     :param alpha: the risk value
+#     :param beta:
+#     :param b0:
+#     :param errs: whether you want to input the data matrix X or the errors
+#     :return: the model and the CVaR
+#     '''
+    n = size(errs)[1]
+    # Create model
+    model = Model(with_optimizer(Gurobi.Optimizer, GRB_ENV))
+    set_optimizer_attribute(model, "OutputFlag", 0)
+
+    # Add variables
+    @variable(model, τ)
+    @variable(model, z[1:n] >= 0)
+
+    # Add objective
+    @objective(model, Min, sum(z)/(α_risk * n) + τ)
+
+    @constraint(model, [1:n], errs .- τ .<= z)
+
+    optimize!(model)
+
+    return objective_value(model)
+end
+
