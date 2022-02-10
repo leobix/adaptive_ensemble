a1b619ebb6c3a9d378a41f9b9613bf064d93c157
Léo Bix <leobix@dhcp-10-29-69-40.dyn.mit.edu>
2022-02-10 16:54:52 -0500

    store in df

--- src/metrics.jl
+++ src/metrics.jl
@@ -10,7 +10,7 @@ function R2_err(err, y_true)
     return 1 - SSR/SST
 end
 
-function get_metrics(err, yt_true)
+function get_metrics(args, method, err, yt_true)
     #TODO ADD saving mechanism
     MAE = mean(err)
     #CVAR_05 = compute_CVaR(err, 0.05)
@@ -24,6 +24,8 @@ function get_metrics(err, yt_true)
     println("R2 : ", R2)
     #println("CVAR 0.05 :", CVAR_05)
     #println("CVAR 0.15 :", CVAR_15)
+    CVAR_05, CVAR_15 = 0, 0
+    add_Dataframe(args, method, MAE, MAPE, RMSE, R2, CVAR_05, CVAR_15)
 end
 
 
@@ -57,3 +59,21 @@ function compute_CVaR(errs, α_risk)
     return objective_value(model)
 end
 
+
+
+function add_Dataframe(args, method, MAE, MAPE, RMSE, R2, CVAR_05, CVAR_15)
+
+#     results = DataFrame(Dataset = String[], End_id = Int64[],
+#         Rho = Float64[], Rho_V = Float64[], Past = Float64[], Num_past = Float64[], Val = Float64[],
+#         Train_test_split = Float64[], Method = String[], MAE = Float64[], MAPE = Float64[], RMSE = Float64[], R2 = Float64[], CVAR_05 = Float64[], CVAR_15 = Float64[])
+
+    results = DataFrame(CSV.File("results.csv"))
+
+    push!(results, (args["data"], args["end-id"], args["rho"], args["rho_V"], args["past"], args["num-past"], args["val"], args["train_test_split"], method, MAE, MAPE, RMSE, R2, CVAR_05, CVAR_15))
+    CSV.write("results.csv", results)
+
+#             CSV.write(filename, df; append=true, writeheader=false)
+#             CSV.write(string("sparsity_", data_set,"_", parameter_range), DataFrame(obj_values))
+#             CSV.write(string("sparsity_", data_set,"_", parameter_range), DataFrame(best_parameters) ; append=true)
+#             CSV.write(string("sparsity_", data_set,"_", parameter_range), DataFrame(all_accuracies) ; append=true)
+end
