15d696de5915128c2c51fe84cad3578fd23a3724
Leonard Boussioux <leobix@mit.edu>
2022-04-27 20:51:14 -0400

    rm useless packages

--- src/main_hypertune.jl
+++ src/main_hypertune.jl
@@ -14,9 +14,9 @@ using Gurobi
 using ArgParse
 using CSV
 using Random
-using Mosek
-using MosekTools
-using Convex
+#using Mosek
+#using MosekTools
+#using Convex
 
 include("eval2.jl")
 include("utils.jl")
